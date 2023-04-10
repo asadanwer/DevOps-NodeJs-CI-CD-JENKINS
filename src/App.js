398342b157c83cf1c6ada74e216a2ffd65cd1924
asadanwer <asad1503b@gmail.com>
2023-04-10 18:23:37 +0500

    asd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        IMRAN KHAN
+        Ab Sirf IK Balle p nishan
       </header>
     </div>
   );
