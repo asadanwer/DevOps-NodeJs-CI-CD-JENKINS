77ada568aa1234a59e311b58c38d80771d11c1a8
asadanwer <asad1503b@gmail.com>
2023-04-10 17:45:54 +0500

    asd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Kinky Talk!!!</p>
+        <p>Smurf</p>
         <a
           className="App-link"
           href="https://reactjs.org"
